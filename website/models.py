26561de611a600d5bfd66143520cd9bd2dd94b49
Namrata <namratadharpale48@gmail.com>
2026-07-28 18:41:21 +0530

    Describe your changes

--- website/models.py
+++ website/models.py
@@ -8,12 +8,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
 
-    LANGUAGE_CHOICES = [
-        ('en', 'English'),
-        ('hi', 'Hindi'),
-        ('mr', 'Marathi'),
-    ]
-
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
@@ -23,12 +17,6 @@ class UserProfile(models.Model):
         upload_to='profile_photos/',
         blank=True,
         null=True
-    )
-
-    language = models.CharField(
-        max_length=10,
-        choices=LANGUAGE_CHOICES,
-        default='en'
     )
 
     dark_mode = models.BooleanField(
@@ -124,58 +112,3 @@ class Prediction(models.Model):
     def __str__(self):
 
         return f"{self.user.username} - {self.prediction_result}"
-
-
-# ==========================================================
-# Free Trial
-# ==========================================================
-
-class GuestPrediction(models.Model):
-
-    session_id = models.CharField(
-        max_length=100
-    )
-
-    no_of_dependents = models.IntegerField()
-
-    education = models.CharField(
-        max_length=20
-    )
-
-    self_employed = models.CharField(
-        max_length=10
-    )
-
-    income_annum = models.FloatField()
-
-    loan_amount = models.FloatField()
-
-    loan_term = models.IntegerField()
-
-
-    cibil_score = models.IntegerField()
-
-    residential_assets_value = models.FloatField()
-
-    commercial_assets_value = models.FloatField()
-
-    luxury_assets_value = models.FloatField()
-
-    bank_asset_value = models.FloatField()
-
-    prediction_result = models.CharField(
-        max_length=20,
-        blank=True
-    )
-
-    prediction_probability = models.FloatField(
-        default=0
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    def __str__(self):
-
-        return f"Guest - {self.session_id}"
